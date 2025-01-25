5bb92d3fc397b91896e53770979e93b4cdfb25c2
gptshubham <shubhamca76@gmail.com>
2025-01-25 11:00:45 +0000

    project setup completed

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+const check = (
+  <h1 class="text-3xl font-bold text-[#f1f2f3] bg-[#333] p-[10px]">Myntra</h1>
+);
+
+root.render(check);
