fa616c9a79984ce845237873d9975ac77d35e99c
gptshubham <shubhamca76@gmail.com>
2025-01-30 02:06:52 +0000

    card flex update

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Header
+// Body
+// Footer
+
+function Card() {
+  return (
+    <div className="border-[2px] border-solid border-gray-200 rounded-[10px] p-[5px]">
+      <img
+        src="https://images.unsplash.com/photo-1627933540891-1fb6a397c89b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGdpcmxzJTIwdHNoaXJ0fGVufDB8fDB8fHww"
+        height="200px"
+        width="150px"
+        className="rounded-[10px]"
+      />
+      <div className="text-center">
+        <h4 className="text-[14px] font-medium">T-Shirts</h4>
+        <h4 className="text-[24px] font-bold">40-80% OFF</h4>
+        <h4 className="text-[14px] font-medium">Shop Now</h4>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    // body
+    <div className="flex gap-[10px] flex-wrap">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-
-const check = (
-  <h1 class="text-3xl font-bold text-[#f1f2f3] bg-[#333] p-[10px]">Myntra</h1>
-);
-
-root.render(check);
+root.render(<App />);
